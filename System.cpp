66e763964caf00c3b9085a9edfad455af7ec057e
Nidish96 <nidbid@gmail.com>
2016-02-07 16:28:55 +0530

    Commit 1
    -Adding class definitions for element and node objects
    -Adding a gitignore file

--- System.cpp
+++ System.cpp
@@ -1,2 +1,19 @@
 #include"System.h"
+#include<cstdio>
+#include<cstdlib>
 
+void ELEMENT::setNodes(int o,NODE N[])
+{
+  O=o;
+  NL = (NODE**)malloc(O*sizeof(NODE*));
+  for( int i=0;i<O;i++ )
+    NL[i] = &N[i];
+  startnode = NL[0]->retid();
+  endnode = NL[O-1]->retid();
+}
+
+void ELEMENT::LPrint(FILE* fid)
+{
+  for( int i=0;i<O;i++ )
+    NL[i]->NPrint(fid);
+}
--- System.h
+++ System.h
@@ -1,4 +1,42 @@
 #ifndef SYSTEM_DEFD
 #define SYSTEM_DEFD
 
+#include<cstdio>
+
+class NODE{
+  int id;
+  double X, F, U; /* Position, Force, Displacement */
+
+public:
+  void setid(int i){id=i;}
+  void setX(double x){X=x;}
+  void setF(double f){F=f;}
+  void setU(double u){U=u;}
+
+  int retid(){return id;}
+  double retX(){return X;}
+  double retF(){return F;}
+  double retU(){return U;}
+
+  void NPrint(FILE* fid){fprintf(fid,"NODE %d: %lf\t%lf\t%lf\n",id,X,F,U);}
+};
+
+class ELEMENT{
+  int id;
+  int O;  /* No. of Nodes in this element */
+  int startnode,endnode;  /* id of start and end nodes */
+  NODE **NL;  /* Pointers to Nodes in the element */
+
+public:
+  void setid(int i){id=i;}
+  void setNodes(int,NODE[]);
+
+  int retid(){return id;}
+  int retO(){return O;}
+  int retstartnode(){return startnode;}
+  int retendnode(){return endnode;}
+
+  void LPrint(FILE*);
+};
+
 #endif

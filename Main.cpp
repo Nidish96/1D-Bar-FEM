fa88c8a24eb1e9d904823630f74b7892b66c6d26
Nidish96 <nidbid@gmail.com>
2016-03-03 17:53:35 +0530

    Commit 17
    Added a basic command line argument based interface.
    Need to add some user interaction capabilities for setting Boundary conditions.

--- Main.cpp
+++ Main.cpp
@@ -14,27 +14,47 @@ double bodyforce(double x)
 
 int main(int args,char* argc[])
 {
+  int i;
   double P = 1.0;
   double L = 1.0;
-  int No_Nodes = (args>1)?atoi(argc[1]):20;
-  int No_Elements = (args>2)?atoi(argc[2]):-1;
-  interval = (args>3)?atoi(argc[3]):100;
-  if( args>4 )
+  int No_Nodes = 20;
+  int No_Elements = -1;
+  interval = 100;
+  int intgn = 1;
+
+  for( i=1;i<args;i++ )
   {
-    for( int i=0;i<sizeof(argc[4]);i++ )
-      argc[4][i] = tolower(argc[4][i]);
-    if( !strcmp(argc[4],"tr") )
-    {
-      Type = Trapezoidal;
-      fprintf(stderr,"TRAPEZOIDAL\n");
+    argc[i][1] = tolower(argc[i][1]);
+    switch(argc[i][1]){
+      case 'n': sscanf(argc[i],"-n%d",&No_Nodes); break;
+      case 'e': sscanf(argc[i],"-e%d",&No_Elements);  break;
+      case 'p': sscanf(argc[i],"-p%lf",&P); break;
+      case 'l': sscanf(argc[i],"-l%lf",&L); break;
+      case 'i': sscanf(argc[i],"-i%d",&intgn);
+                switch(intgn){
+                  case 0: Type = Trapezoidal; break;
+                  case 1: Type = GaussLegendre; break;
+                  case 2: Type = GaussLegendreIso;  break;} break;
+      case 'v': sscanf(argc[i],"-v%d",&interval);  break;
+      case 'r': break;
+      case 'h': i=100;  break;
     }
-    else if( !strcmp(argc[4],"gl") )
-    {
-      Type = GaussLegendre;
-      fprintf(stderr,"GAUSS-LEGENDRE\n");
-    }
-    else
-      Type = GaussLegendreIso;
+  }
+  if( i==101||args==1 )
+  {
+    fprintf(stderr,"\n\tFinite Element Solver for 1D Bar Element\n");
+    fprintf(stderr,"\tArgs  \tType    \tDescription\n");
+    fprintf(stderr,"\t-n    \tint     \tNumber of Nodes [%d]\n",No_Nodes);
+    fprintf(stderr,"\t-e    \tint     \tNumber of Elements (-1 implies Nodes-1) [%d]\n",No_Elements);
+    fprintf(stderr,"\t-l    \tfl      \tLength of Element in metres [%lf]\n",L);
+    fprintf(stderr,"\t-i    \t<0,1,2> \tType of Integration [%d]\n",intgn);
+    fprintf(stderr,"\t      \t        \t0 - Trapezoidal\n");
+    fprintf(stderr,"\t      \t        \t1 - Gauss-Legendre \n");
+    fprintf(stderr,"\t      \t        \t2 - Gauss-Legendre with Isoparametric Mapping\n");
+    fprintf(stderr,"\t-v    \tint     \tNumber of Intervals (for Trapezoidal integration) [%d]\n",interval);
+    fprintf(stderr,"\t-r    \t-       \tRun without default Parameters\n");
+    fprintf(stderr,"\t-h    \t-       \tPrint Usage Message\n");
+    exit(0);
   }
 
   SYSTEM SYS(1,No_Nodes,L);
--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@ debug: $(DEPS)
 	$(CC) -o Main $(OBJ) $(CFLAGS) $(DBG)
 
 plot: Plot.gp
-	./Main $(Nodes) $(Elements) $(Interval) $(Intg)>OUTPUT.dat && gnuplot5 Plot.gp -p
+	./Main -n$(Nodes) -e$(Elements) -v$(Interval) -i$(Intg)>OUTPUT.dat && gnuplot5 Plot.gp -p
 
 clean:
 	rm *.o Main
